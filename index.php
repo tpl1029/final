3a38bdfad494fc060dc43f4b36c44ee619108d6a
Antonio Jennings <Jenningsantonio17@gmail.com>
2020-12-16 11:38:48 -0500

    Finished Home Page and CSS

--- index.php
+++ index.php
@@ -19,15 +19,39 @@
      <!-- Website Information -->
         <div class="info-grid">
             <div class="service">   
-                <img src="./View/Public/Images/service.jpg">
+                <img src="./View/Public/Images/service.jpg" class="img-service"> 
+            </div>
+
+            <div class="serivce-info">
+                <p class="p-services" id="style-font">Welcome to Fortisure Repairs, where we make sure that our customers recieve the best customer services and are provided with the help they need!</p>
             </div>
 
             <div class="repair">
-                <img src="./View/Public/Images/repair.jpg">
+                <img src="./View/Public/Images/repair.jpg" class="img-repair">
+            </div>
+
+            <div class="repair-info">
+                <p class="p-repairs" id="style-font">We do all of sorts of repairs on cars:</p>
+                <br>
+                    <ul class="p-repairs" id="style-font">
+                        <li>Oil/oil filter changed.</li>
+                        <li>Wiper blades replacement.</li>
+                        <li>Replace air filter.</li>
+                        <li>Scheduled maintenance.</li>
+                        <li>New tires.</li>
+                        <li>Battery replacement.</li>
+                        <li>Brake work.</li>
+                        <li>And More!</li>
+                    </ul>
             </div>
 
             <div class="paper">
-                <img src="./View/Public/Images/paper.jpg">
+                <img src="./View/Public/Images/paper.jpg" class="img-paper">
+            </div>
+
+            <div class="paper-info">
+                <p class="p-papers" id="style-font">As we do our repairs on vehicles, we make sure that everything works perfectly! 
+                And we even provide a way for our customers to check and recieve any status updates during the repairs!</p>
             </div>
         </div>
 
